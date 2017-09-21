8176cbf65e74a2fe108bab99becb9d6ee99e6b90
AfroMonkey <moisalejandro@gmail.com>
2017-09-21 17:05:45 -0500

    [INIT]

---
+++ README.md
@@ -0,0 +1,4 @@
+Pricelist Gain
+==============
+
+TODO [IMP]
---
+++ models/product_pricelist_item.py
@@ -0,0 +1,20 @@
+from odoo import api, fields, models
+
+
+class ProductPricelistItem(models.Model):
+    _inherit = 'product.pricelist.item'
+
+    gain = fields.Float(
+        compute='_get_gain', readonly=False,
+    )
+    price_discount = fields.Float('Price Discount', default=0, digits=(16, 2), compute='_get_price_discount', store=True, readonly=False)
+
+    @api.depends('price_discount')
+    @api.multi
+    def _get_gain(self):
+        self.gain = 100*self.price_discount / (self.price_discount - 100)
+
+    @api.depends('gain')
+    @api.multi
+    def _get_price_discount(self):
+        self.price_discount = 100*self.gain / (self.gain - 100)
