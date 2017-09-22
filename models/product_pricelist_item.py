4f5760010dd6f9a5df97dda5d8ff66f05b04c67f
AfroMonkey <moisalejandro@gmail.com>
2017-09-22 16:42:16 -0500

    [FIX] api.multi -> api.one

--- models/product_pricelist_item.py
+++ models/product_pricelist_item.py
@@ -10,11 +10,11 @@ class ProductPricelistItem(models.Model):
     price_discount = fields.Float('Price Discount', default=0, digits=(16, 2), compute='_get_price_discount', store=True, readonly=False)
 
     @api.depends('price_discount')
-    @api.multi
+    @api.one
     def _get_gain(self):
         self.gain = 100*self.price_discount / (self.price_discount - 100)
 
     @api.depends('gain')
-    @api.multi
+    @api.one
     def _get_price_discount(self):
         self.price_discount = 100*self.gain / (self.gain - 100)
